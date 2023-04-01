b817d279cfbc80ab8a7afbea6ed1bc445708a464
Ayrat <ayropp@yandex.ru>
2023-04-01 09:27:10 +0300

    del: unused imports

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
-import ru.yandex.practicum.filmorate.storage.dao.InDbMpaStorage;
 
-import java.util.Collection;
 import java.util.List;
 
 
