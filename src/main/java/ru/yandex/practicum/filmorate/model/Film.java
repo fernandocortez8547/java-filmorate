874ccfd3072ef1e431a18187041390c8aa03668b
fernandocortez8547 <108097219+fernandocortez8547@users.noreply.github.com>
2023-04-03 08:08:14 +0300

    Revert "Add database"

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,28 +1,33 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import ru.yandex.practicum.filmorate.annotation.AfterThan;
 
 import javax.validation.constraints.*;
-import java.time.LocalDate;
-import java.util.Set;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 @EqualsAndHashCode(exclude = {"id"})
-@Getter
-@Setter
-@Builder(toBuilder = true)
-@AllArgsConstructor
-@ToString
 public class Film {
-    private int id;
+    private int id = 0;
     @NotBlank(message = "название фильма не должно быть пустым")
     private final String name;
-    @Size(max = 200)
+    @Size(max=200)
     private final String description;
     @AfterThan
     private final LocalDate releaseDate;
-    @Positive(message = "длительность фильма должна быть больше 0")
+    @Positive (message = "длительность фильма должна быть больше 0")
     private final int duration;
-    private Mpa mpa;
-    private Set<Genre> genres;
+
+    private List<Integer> likesList = new ArrayList<>();
+
+    public void addLike(int id) {
+        likesList.add(id);
+    }
+
+    public void removeLike(Integer id) { likesList.remove(id); }
 }
